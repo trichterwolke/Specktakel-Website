5f350326b3437a330c628b61eeb28aac2d0ac19b
trichterwolke <trichterwolke@outlook.com>
2019-01-13 17:13:29 +0100

    hero diashow

--- themes/specktakel/static/js/specktakel.js
+++ themes/specktakel/static/js/specktakel.js
@@ -28,21 +28,21 @@
     });
 
     // Fit Text Plugin for Main Header   
-    $("header h1").fitText(3,
+    $(".header-content h1").fitText(3,
         {
             minFontSize: '20px',
             maxFontSize: '36px'
         }
     );
 
-    $("header h2 p").fitText(0.8,
+    $(".header-content h2 p").fitText(0.8,
         {
             minFontSize: '50px',
             maxFontSize: '120px'
         }
     );
 
-    $("header h3").fitText(1.2,
+    $(".header-content h3").fitText(1.2,
         {
             minFontSize: '36px',
             maxFontSize: '60px'
@@ -56,6 +56,18 @@
         }
     })
 
+    $('#btn-header-left').click(function () {
+        autoSlideOn = false;
+        plusSlides(-1);
+    });
+
+    $('#btn-header-right').click(function () {
+        autoSlideOn = false;
+        plusSlides(1);
+    });
+
+    setInterval(autoSlide, 5000);
+
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
@@ -64,3 +76,67 @@
     var lon = parseFloat($('#longitude').val());
     new SlippyMap(lat, lon).init();
 })(jQuery); // End of use strict
+
+var slideIndex = 1;
+var slideCount = 6;
+var autoSlideOn = true;
+
+showSlide(slideIndex);
+
+function autoSlide(){
+    if(autoSlideOn)
+        plusSlides(1);    
+}
+
+function plusSlides(n) {
+    slideIndex += n;
+
+    if (slideIndex < 1) {
+        slideIndex = slideCount;
+    }
+    else if (slideIndex > slideCount) {
+        slideIndex = 1
+    }
+
+    showSlide(slideIndex);
+    showHeader(slideIndex);
+}
+
+function showHeader(index){
+    if(index == 1) {        
+        $(".header-content").addClass("fade-in");
+        $(".header-content-alt").removeClass("fade-in");
+    } else {        
+        $(".header-content").removeClass("fade-in");
+        $(".header-content-alt").addClass("fade-in");
+    }
+}
+
+function getNextIndex(index) {
+    if (index >= slideCount) {
+        return 1
+    }
+    else {
+        return slideIndex + 1;
+    }
+}
+
+function showSlide(index) {
+    loadImage(index);
+    loadImage(getNextIndex(index));
+    fadeIn(index);
+}
+
+function loadImage(index) {
+    if ($('#hero-img-' + index).length == 0) {
+        var url = '../../../../hero/hero' + index + '.jpg';
+        var element = '<img id="hero-img-' + index + '" class="hero-img" src="' + url + '"/>';
+        $('#hero-img-container').append(element);
+    }
+}
+
+function fadeIn(index) {
+    var oldImage = $('.hero-img.fade-in');
+    $('#hero-img-' + index).addClass('fade-in');
+    oldImage.removeClass('fade-in');
+}
